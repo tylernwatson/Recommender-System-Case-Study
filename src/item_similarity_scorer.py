c0357fe4dac31179bd481811fa6c8ff5368dbb9e
tyler.n.watson@gmail.com <tyler.n.watson@gmail.com>
2017-12-18 14:28:13 -0600

    Item similarity code update and new image

--- src/item_similarity_scorer.py
+++ src/item_similarity_scorer.py
@@ -32,11 +32,12 @@ def score(df_true, df_predict):
 
     return df_true[top_5==1].mean()['true_rating']
 
-def create_factorization_recommender(sf, only_top_k=64):
+def create_factorization_recommender(sf, similarity_type='jaccard', threshold=.001, only_top_k=64):
     m = gl.recommender.item_similarity_recommender.create(observation_data=sf,
                                                         user_id='user_id', item_id='joke_id',
                                                         target='rating',
-                                                        similarity_type='pearson',
+                                                        similarity_type=similarity_type,
+                                                        threshold=threshold,
                                                         only_top_k=only_top_k)
     return m
 
@@ -52,26 +53,50 @@ if __name__ == "__main__":
     df_true['joke_id'] = val_data['joke_id']
     df_true['true_rating'] = val_data['rating']
 
-    # Plot scores vs. num_factors
-
-    hyperparameters = [num_factors, similarity_type_list, thresholds, only_top_k_list]
-    only_top_k = list(np.linspace(64,96,num=17))
-    threshold = list(np.linspace(0.0005,.01,num=20))
+    # Plot scores vs. tuned hyperparameters
     similarity_type = ['pearson', 'cosine', 'jaccard']
-    num_factors = range(51)
     scores=[]
-    for h in hyperparameters:
-        for i in h:
-            m = create_factorization_recommender(train_data, i)
-            df_predict['pred_rating'] = m.predict(val_data)
-            rc= score(df_true, df_predict)
-            scores.append(rc)
-            print(h,': ', k, 'Score: ', rc)
-        plt.plot(h, scores)
-        plt.title('Score vs ', h)
-        plt.xlabel(h)
-        plt.ylabel('Score')
-        plt.show()
+    for i in similarity_type:
+        m = create_factorization_recommender(train_data, similarity_type=i)
+        df_predict['pred_rating'] = m.predict(val_data)
+        rc= score(df_true, df_predict)
+        scores.append(rc)
+        print('Similarity Type: ', i, 'Score: ', rc)
+    plt.bar(np.arange(3), scores)
+    plt.xticks(np.arange(3), ('Pearson', 'Cosine', 'Jaccard'))
+    plt.title('Score vs Similarity Type')
+    plt.xlabel('Similarity Type')
+    plt.ylabel('Score')
+    plt.show()
+
+    threshold = list(np.linspace(0.0005,.01,num=20))
+    scores=[]
+    for i in h:
+        m = create_factorization_recommender(train_data, threshold=i)
+        df_predict['pred_rating'] = m.predict(val_data)
+        rc= score(df_true, df_predict)
+        scores.append(rc)
+        print('Threshold: ', i, 'Score: ', rc)
+    plt.plot(threshold, scores)
+    plt.title('Score vs Threshold')
+    plt.xlabel('Threshold')
+    plt.ylabel('Score')
+    plt.show()
+
+    only_top_k = list(np.linspace(64,96,num=17))
+    scores=[]
+    for i in h:
+        m = create_factorization_recommender(train_data, only_top_k=i)
+        df_predict['pred_rating'] = m.predict(val_data)
+        rc= score(df_true, df_predict)
+        scores.append(rc)
+        print('Only Top K: ', i, 'Score: ', rc)
+    plt.plot(only_top_k, scores)
+    plt.title('Score vs Only Top K')
+    plt.xlabel(only_top_k)
+    plt.ylabel('Score')
+    plt.show()
+
 
     # sample_sub_fname = "data/sample_submission.csv"
     # ratings_data_fname = "data/ratings.dat"
